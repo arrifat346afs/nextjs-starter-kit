2c943c7b7cf0851bf240fa609ca73c5cb63d4205
Abdur Rahman Rifat <arrifat1235@gmail.com>
2025-05-21 12:41:15 +0600

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -9,9 +9,7 @@ export default clerkMiddleware(async (auth, req) => {
 
   const token = (await (await auth()).getToken({ template: "convex" }))
 
-
-  const { hasActiveSubscription } = await fetchQuery(api.subscriptions.getUserSubscriptionStatus, {
-  }, {
+  const { hasActiveSubscription } = await fetchQuery(api.subscriptions.getUserSubscriptionStatus, {}, {
     token: token!,
   });
 
@@ -31,6 +29,6 @@ export const config = {
     // Skip Next.js internals and all static files, unless found in search params
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     // Always run for API routes
-
+    '/(api|trpc)(.*)',
   ],
 }
